46fe679e72f03ed1fa240d280d7110ba77b1588d
saifkazi25 <m.saifkazi@gmail.com>
2025-07-07 18:59:12 +0300

    Update route.ts

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -10,7 +10,8 @@ export async function POST(req: NextRequest) {
   const { prompt, imageData } = body;
 
   const modelOutput = await replicate.run(
-    `${process.env.SDXL_VERSION}`, // SDXL model version from .env.local
+    "stability-ai/sdxl:7762fd07cf82c948538e41f63f77d685e02b063e37e496e96eefd46c929f9bdc",
+, // SDXL model version from .env.local
     {
       input: {
         prompt: prompt,
